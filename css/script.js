4d6f82047591d49db80e7742fef70984ebf0e336
Gabriel Thuku <gabriel.makumi@student.moringaschool.com>
2021-04-12 18:46:17 +0300

    add stlyes, script styles, modified html

--- css/script.js
+++ css/script.js
@@ -1,3 +1,21 @@
-p3 {
- var year = "1970, 2021";
+function getDayName() {
+  alert("Hello,click OK to continue");
+  let dateString = prompt("Enter date of birth: dd/mm/yyyy");
+  let gender = prompt("What is your gender, male or female?");
+  getNameFromDate(dateString, gender);
+}
+function getNameFromDate(dateString, gender) {
+  let dateOfBirth = new Date(dateString); //create a date object from the string
+  let dayOfTheWeek = dateOfBirth.getDay();
+  let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
+  let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  if(gender == "male") {
+      //console.log(maleNames[dayOfTheWeek]);
+      document.getElementById("showNameHere").innerText = "Your Birth Name is " + maleNames[dayOfTheWeek];
+  }
+  else if(gender == "female") {
+    //console.log(femaleNames[dayOfTheWeek]);
+    document.getElementById("showNameHere").innerText = "Your Birth Name is " + femaleNames[dayOfTheWeek];
+  }
+
 }
